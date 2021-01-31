b8b6d916d5d51da1171fcbc7d63b8c119072fdbf
BuildTools <unconfigured@null.spigotmc.org>
2021-01-30 17:35:52 -0700

    learning

--- content.js
+++ content.js
@@ -1,3 +1,10 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request.message)
+        if (request.message === "clicked_browser_action") {
+            var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-console.log(firstHref);
+            alert(firstHref);
+        }
+    }
+);
