e721efcf7c39ed5fe9eb1b3e63526763943015dd
Daniel <daniel.r.wakefield@aggiemail.usu.edu>
2021-01-30 19:08:30 -0700

    added pop up and broken search bar

--- content.js
+++ content.js
@@ -1,10 +1,22 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(request.message)
-        if (request.message === "clicked_browser_action") {
-            var firstHref = $("a[href^='http']").eq(0).attr("href");
+        // if (request.message === "clicked_browser_action") {
+        //     var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-            alert(firstHref);
-        }
+        //     alert(firstHref);
+
+        // // new line 
+        // chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref})
+        // }
     }
 );
+// function runner() {
+    // alert("YEET")
+    $(".butt").on("click",() => {
+        alert("YEET")
+    })
+// }
+
+// $(document).ready(runner)
+
